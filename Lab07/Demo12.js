e142899e98afe3043bb6659fba40a50921152503
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2021-10-09 19:55:08 +0700

    refactor: module

--- Lab07/Demo12.js
+++ Lab07/Demo12.js
@@ -7,13 +7,12 @@
 console.clear()
 
 const func = require('./Demo12_module')
-const say = require('./Demo12_module_s')
-
+const {sayHelloWorld} = require('./Demo12_module_s')
 
 let a = 4 
 let b = 5
 
-say.sayHelloWorld()
+sayHelloWorld()
 
 console.log(func.cong(a, b))
 console.log(func.tru(a, b))
